d7255727bd5d0f83919ad00a32ea6583096388df
sseidmed <sseidmed@gmail.com>
2021-02-04 13:56:39 -0500

    Add an input form

--- src/App.vue
+++ src/App.vue
@@ -2,28 +2,47 @@
   <div id="app" class="small-container">
     <h1>Employees</h1>
 
-    <employee-table />
+    <employee-form />
+    <employee-table v-bind:employees="employees" />
   </div>
 </template>
 
 <script>
 import EmployeeTable from './components/EmployeeTable.vue'
+import EmployeeForm from './components/EmployeeForm.vue'
 
 export default {
   name: 'App',
   components: {
-    EmployeeTable
-  }
+    EmployeeTable,
+    EmployeeForm,
+  },
+  data() {
+    return {
+      employees: [
+        {
+          id: 1, 
+          name: 'Richard Hendricks', 
+          email: 'richard@piedpiper.com',
+        },
+        {
+          id: 2,
+          name: 'Bertram Gilfoyle',
+          email: 'gilfoyle@piedpiper.com',
+        },
+        {
+          id: 3,
+          name: 'Dinesh Chugtai',
+          email: 'dinesh@piedpiper.com',
+        },
+      ],
+    }
+  },
 }
 </script>
 
 <style>
 #app {
-  button {
-    background: #009435;
-    border: 1px solid #009435;
-  }
-
   .small-container {
     max-width: 680px;
   }
---
+++ src/components/EmployeeForm.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="employee-form">
+        <form>
+            <label>Employee Name</label>
+            <input type="text" />
+            <label>Employee Email</label>
+            <input type="text" />
+            <button>Add Employee</button>
+            </form>
+        </div>
+    
+</template>
+
+<script>
+export default {
+    name: 'employee-form',
+    data() {
+        return {
+            employee: {
+                name: '',
+                email: '',
+            },
+        }
+    },
+}
+</script>
+
+<style scoped>
+    form {
+        margin-bottom: 2rem;
+    }
+    button {
+        background-color: green;
+    }
+</style>
